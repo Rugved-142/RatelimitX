8026c97c192db02b299095d6b4ca9e53cbbe2544
Rugved-142 <gundawar.r@northeastern.edu>
2026-01-09 07:26:43 -0500

    Add admin API endpoints for rate limiter management

--- src/main/java/com/ratelimitx/core/controller/ApiController.java
+++ src/main/java/com/ratelimitx/core/controller/ApiController.java
@@ -1,6 +1,5 @@
 package com.ratelimitx.core.controller;
 
-import com.ratelimitx.core.service.RateLimiterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ratelimitx.core.service.RateLimiterService;
 
 
 
@@ -20,7 +21,7 @@ public class ApiController {
 
     @GetMapping("/data")
     public ResponseEntity<String> getData( @RequestHeader(value="X-API-Key", defaultValue="anonymous") String apiKey){
-        if(!rateLimiterService.checkRateLimit(apiKey, 10)){
+        if(!rateLimiterService.checkCustomRateLimit(apiKey)){
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Rate limit exceeded");
         }
         return ResponseEntity.ok("Success! Here's yourData");
--- src/main/java/com/ratelimitx/core/service/RateLimiterService.java
+++ src/main/java/com/ratelimitx/core/service/RateLimiterService.java
@@ -6,8 +6,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 
-
-
 @Service
 public class RateLimiterService {
     
@@ -24,5 +22,12 @@ public class RateLimiterService {
             redisTemplate.expire(key,60,TimeUnit.SECONDS);
 
         return count <= maxRequest;
+    }
+
+    public boolean checkCustomRateLimit(String userId){
+        String customLimit = (String) redisTemplate.opsForHash().get("user-limits",userId);
+        int maxRequest = customLimit !=null ? Integer.parseInt(customLimit) : 10;
+
+        return checkRateLimit(userId, maxRequest);
     }
 }
